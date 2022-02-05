6de4aa4ff9c2de63ba0e41f7e3d03c2c04d04169
NabilMouami <86678449+NabilMouami@users.noreply.github.com>
2022-02-05 15:52:08 +0100

    Create Listdata.js

---
+++ frontend/src/components/Library/Listdata.js
@@ -0,0 +1,48 @@
+import logo1 from "./images/rich.jpg"
+import logo2 from "./images/thing.jpg"
+import logo3 from "./images/heaven.jpg"
+import logo4 from "./images/antigone.jpg"
+
+const box = [
+    {
+      id: 1,
+      image: logo1,
+      title: "rich dad poor dad",
+      autor: "Robert Kiyosaki",
+      page: 336,
+      info: "Père riche, père pauvre est un livre de Robert Kiyosaki et de Sharon Lechter paru en 1997. De style autobiographique, Robert Kiyosaki utilise un ensemble de paraboles et d'exemples tirés de son propre parcours afin de souligner l'importance de développer son intelligence financière. Il y explique comment l'investissement, l'immobilier, la création et l'acquisition d'entreprises peuvent être utilisés pour construire sa richesse et devenir financièrement indépendant.",
+      view: 234,
+      vote: 3,
+    },
+    {
+        id: 2,
+        image: logo2,
+        title: "Think and Grow Rich",
+        autor: "Napoleon Hill",
+        page: 238,
+        info: "Nouvelle traduction inédite. L'un des best-sellers les plus influents de tous les temps : vendu à plus de 60 millions d'exemplaires dans le monde ! « Lorsque vous dressez votre plan pour gagner votre part de richesse, ne vous laissez pas influencer par ceux qui se moquent de vos rêves...",
+        view: 500,
+        vote: 14,
+    },
+    {
+        id: 3,
+        image: logo3,
+        title: "As bright As heaven",
+        autor: "Susan Meissner",
+        page: 196,
+        info: "L'histoire du chagrin et de l'espoir d'une famille. Forts [et] résilients, les personnages de Meissner sortent de la page et entrent dans l'histoire. »—Lisa Wingate, auteure à succès du New York Times de Before We Were Yours",
+        view: 145,
+        vote: 5,
+    },
+    {
+        id: 4,
+        image: logo4,
+        title: "Antigone",
+        autor: "Jean Anouilh",
+        page: 128,
+        info: "Antigone est une pièce en un acte de Jean Anouilh représentée pour la première fois au théâtre de l'Atelier à Paris le 4 février 1944, durant l'Occupation allemande, dans une mise en scène, des décors et des costumes d'André Barsacq. Réécriture de la pièce éponyme de Sophocle, elle fait partie de la série des Nouvelles Pièces noires, avec Jézabel (1932), Roméo et Jeannette (1946) et Médée (1953).",
+        view: 209,
+        vote: 8,
+    },
+];
+export default box; 
